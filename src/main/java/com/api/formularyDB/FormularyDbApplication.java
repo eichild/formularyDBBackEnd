cd009b6d5f2bf478a8ddd3bee85e42b172ff9248
eichild <lucas.gregorio@totvs.com.br>
2022-05-09 14:43:14 -0300

    adicionado estrutura de projeto e configuração de conexão ao banco

---
+++ src/main/java/com/api/formularyDB/FormularyDbApplication.java
@@ -0,0 +1,13 @@
+package com.api.formularyDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormularyDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormularyDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/formularyDB/FormularyDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.formularyDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormularyDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
