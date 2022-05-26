53517633e3ec8ed1a7b289e60a8e61c31a123e45
eichild <lucas.gregorio@totvs.com.br>
2022-05-26 17:01:58 -0300

    adicionado swagger de aplicação

--- src/main/java/com/api/formularyDB/repositories/TipoBancoDeDadosRepository.java
+++
@@ -1,6 +0,0 @@
-package com.api.formularyDB.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TipoBancoDeDadosRepository extends JpaRepository<TipoBancoDeDadosModel, Integer> {
-}
